2817703cdec3e3bbdf954b66e7e43331453c1309
JuliaMathias <juliamss@gmail.com>
2023-03-29 20:41:17 -0300

    mix format

--- test/investment_tracker_web/controllers/error_json_test.exs
+++ test/investment_tracker_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule InvestmentTrackerWeb.ErrorJSONTest do
   use InvestmentTrackerWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert InvestmentTrackerWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert InvestmentTrackerWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
