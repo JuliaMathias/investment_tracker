0644299b675c83f92f14bfe8bdc76d5f845bcbea
JuliaMathias <juliamss@gmail.com>
2023-03-31 23:54:12 -0300

    add :nimble_csv

--- mix.exs
+++ mix.exs
@@ -64,7 +64,8 @@ defmodule InvestmentTracker.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.10", only: :test}
+      {:excoveralls, "~> 0.10", only: :test},
+      {:nimble_csv, "~> 1.1"}
     ]
   end
 
