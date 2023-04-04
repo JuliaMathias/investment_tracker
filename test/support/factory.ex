ae2e88cf47b293dddbee37422bf6a7a7fa0c93f8
JuliaMathias <juliamss@gmail.com>
2023-04-04 16:12:21 -0300

    add docs

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,4 +1,7 @@
 defmodule InvestmentTracker.Factory do
+  @moduledoc """
+  Factories to help us with tests.
+  """
   use ExMachina.Ecto, repo: InvestmentTracker.Repo
   use InvestmentTracker.Factory.CSVs
   use InvestmentTracker.Factory.Wallet
